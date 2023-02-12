6055cd3df9b74394e2fd6c7411deaae0c5652ca2
Aleksey_Filkin <acid_wind@mail.ru>
2023-02-12 15:00:36 +0700

    SumOfOddElementInArrayTask solved

--- SumOfOddElementInArrayTask/Program.cs
+++ SumOfOddElementInArrayTask/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0*/
+
+Console.Write("Введите размерность массива: ");
+int n = int.Parse(Console.ReadLine() ?? "");
+int sum = 0;
+int[] array = new int[n];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 10);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+for (int i = 1; i < array.Length; i+=2)
+{
+    sum +=array[i];
+    
+}
+Console.WriteLine($"Сумма элементов на нечетных позициях в массиве = {sum}");
